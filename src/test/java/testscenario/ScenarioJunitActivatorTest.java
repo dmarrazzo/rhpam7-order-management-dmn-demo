f5758a44b1259963e00ce2d59ca9f43dcb47f721
Donato Marrazzo <donato.marrazzo@gmail.com>
2019-07-31 12:07:14 +0200

    Activator new package

--- src/test/java/testscenario/ScenarioJunitActivatorTest.java
+++ src/test/java/testscenario/ScenarioJunitActivatorTest.java
@@ -1,7 +1,10 @@
 package testscenario;
+
+import org.drools.scenariosimulation.backend.runner.ScenarioJunitActivator;
+
 /**
 * Do not remove this file
 */
-@org.junit.runner.RunWith(org.drools.workbench.screens.scenariosimulation.backend.server.runner.ScenarioJunitActivator.class)
+@org.junit.runner.RunWith(ScenarioJunitActivator.class)
 public class ScenarioJunitActivatorTest {
 }
